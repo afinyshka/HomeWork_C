bdae7efd18ef5b799f928a068d7acee81fa9fff6
afinyshka <afinyshka@mail.ru>
2022-04-27 11:13:01 +0300

    Even number

---
+++ HW_1.003_Even_number/Program.cs
@@ -0,0 +1,5 @@
+﻿// Выяснить является ли число чётным (0 - является четным числом)
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number % 2 == 0) Console.WriteLine($"Число {number} четное! ");
+else Console.WriteLine($"Число {number} нечетное! ");
