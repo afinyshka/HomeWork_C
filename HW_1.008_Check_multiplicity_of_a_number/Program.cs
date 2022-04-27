77de23072fb94cf2bef3ae39639cd22bfa6696c3
afinyshka <afinyshka@mail.ru>
2022-04-27 23:01:28 +0300

    Check multiplicity of a number

---
+++ HW_1.008_Check_multiplicity_of_a_number/Program.cs
@@ -0,0 +1,8 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number1%number2 == 0) Console.WriteLine($"Число {number1} кратно числу {number2} ");
+else if (number2%number1 == 0) Console.WriteLine($"Число {number2} кратно числу {number1} ");
+else Console.WriteLine($"Числа {number1} и {number2} не кратны ");
