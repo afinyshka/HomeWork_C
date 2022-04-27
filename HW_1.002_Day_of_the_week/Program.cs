5e8404cbef364d48cdedd81c2d8e2449b81aaf9e
afinyshka <afinyshka@mail.ru>
2022-04-27 10:59:49 +0300

    add another programm idea

--- HW_1.002_Day_of_the_week/Program.cs
+++ HW_1.002_Day_of_the_week/Program.cs
@@ -9,3 +9,14 @@ if (day==5) Console.WriteLine("Пятница ");
 if (day==6) Console.WriteLine("Суббота ");
 if (day==7) Console.WriteLine("Воскресенье ");
 if (day<1 || day>7) Console.WriteLine("Такого дня недели не существует! ");
+
+//Console.WriteLine("Введите номер дня недели: ");
+//int day = int.Parse(Console.ReadLine());
+//if (day==1) Console.WriteLine("Понедельник ");
+//else if (day==2) Console.WriteLine("Вторник ");
+//else if (day==3) Console.WriteLine("Среда ");
+//else if (day==4) Console.WriteLine("Четверг ");
+//else if (day==5) Console.WriteLine("Пятница ");
+//else if (day==6) Console.WriteLine("Суббота ");
+//else if (day==7) Console.WriteLine("Воскресенье ");
+//else Console.WriteLine("Такого дня недели не существует! ");
