d2562439ba7aee709538db5374da32e6264747f1
afinyshka <afinyshka@mail.ru>
2022-04-27 22:21:38 +0300

    Delete second digit in 3 digits number

---
+++ HW_1.007_Delete_2nd_digit_in_3dig_n/Program.cs
@@ -0,0 +1,10 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Введите трёхзначное число: ");
+int n = int.Parse(Console.ReadLine());
+if (n > 99 && n < 1000 || n > -1000 && n < -99)
+{
+Console.Write($"Полученное двузначное число = {n/100}");
+Console.WriteLine(Math.Abs(n%10));
+}
+else 
+Console.WriteLine("Введено не трёхзначное число! ");
