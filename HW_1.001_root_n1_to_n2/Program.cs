44126a308462b16a3c4afa44dad0638319999e7d
afinyshka <afinyshka@mail.ru>
2022-04-27 10:14:47 +0300

    add problem situation

--- HW_1.001_root_n1_to_n2/Program.cs
+++ HW_1.001_root_n1_to_n2/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите первое число:");
+﻿//По двум заданным числам проверять является ли первое квадратом второго:
+Console.WriteLine("Введите первое число:");
 int n1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число:");
 int n2 = int.Parse(Console.ReadLine());
