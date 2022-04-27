c5012385f56ffaa03c0d454126bf4eeb88f582e3
afinyshka <afinyshka@mail.ru>
2022-04-27 11:18:14 +0300

    special outprint for 0

--- HW_1.003_Even_number/Program.cs
+++ HW_1.003_Even_number/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Выяснить является ли число чётным (0 - является четным числом)
 Console.WriteLine("Введите число: ");
 int number = int.Parse(Console.ReadLine());
-if (number % 2 == 0) Console.WriteLine($"Число {number} четное! ");
+if (number == 0) Console.WriteLine($"Как ни странно, но число {number} тоже четное! ");
+else if (number % 2 == 0) Console.WriteLine($"Число {number} четное! ");
 else Console.WriteLine($"Число {number} нечетное! ");
