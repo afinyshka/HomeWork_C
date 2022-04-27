398820352ac4e73c8ca8df98a5e81c8b4a007940
afinyshka <afinyshka@mail.ru>
2022-04-28 00:15:15 +0300

    add remainder outprint

--- HW_1.008_Check_multiplicity_of_a_number/Program.cs
+++ HW_1.008_Check_multiplicity_of_a_number/Program.cs
@@ -5,4 +5,9 @@ Console.WriteLine("Введите второе число: ");
 int number2 = int.Parse(Console.ReadLine());
 if (number1%number2 == 0) Console.WriteLine($"Число {number1} кратно числу {number2} ");
 else if (number2%number1 == 0) Console.WriteLine($"Число {number2} кратно числу {number1} ");
-else Console.WriteLine($"Числа {number1} и {number2} не кратны ");
+else 
+{
+    Console.Write($"Числа {number1} и {number2} не кратны, ");
+    if (number1>number2) Console.WriteLine($"остаток от деления {number1} на {number2} = {number1%number2}. ");
+    else Console.WriteLine($"остаток от деления {number2} на {number1} = {number2%number1}. ");
+}
