ea6df2b09f22182763d985ce8cbc9ce4dc6a4358
afinyshka <afinyshka@mail.ru>
2022-04-27 13:11:01 +0300

    Max digit from random number

---
+++ HW_1.006_Max_from_Random/Program.cs
@@ -0,0 +1,9 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+Console.WriteLine("Hello, World!");
+int n = new Random().Next(10,100);
+Console.WriteLine($"Случайно выбранное число из интервала [10, 99] = {n} ");
+int d1 = n/10;
+int d2 = n%10;
+int maxDigit = d1;
+if (d2>d1) maxDigit = d2;
+Console.WriteLine($"Наибольшая цифра числа: {maxDigit} ");
