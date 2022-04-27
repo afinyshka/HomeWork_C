9348b26a4142b49ca27e506a5b59e990abe8e631
afinyshka <afinyshka@mail.ru>
2022-04-27 12:02:00 +0300

    Even number 1-N

---
+++ HW_1.004_Even_numbers_1-N/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать четные числа от 1 до N
+Console.WriteLine("Введите число > 1: ");
+int n = int.Parse(Console.ReadLine());
+int n1 = 1;
+if (n < 1) Console.WriteLine("Введённое значение не удовлетворяет условию 'Введите число > 1' ");
+else Console.WriteLine($"Четные числа в интервале от 1 до {n}: ");
+while (n1 <= n)
+{
+  if (n1%2 == 0)
+  {
+  Console.Write($"{n1} ");
+  }
+n1++;
+}
