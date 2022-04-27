b2e777ffe8a6bf9300698e20e1f2c3e54a4563b4
afinyshka <afinyshka@mail.ru>
2022-04-27 10:10:12 +0300

    001 Root check n1 to n2

---
+++ HW_1.001_root_n1_to_n2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число:");
+int n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int n2 = int.Parse(Console.ReadLine());
+if (n1 == n2*n2)
+{
+    Console.WriteLine("Первое число является квадратом второго числа. ");
+}
+else
+{
+    Console.WriteLine("Первое число НЕ является квадратом второго числа! ");
+}
