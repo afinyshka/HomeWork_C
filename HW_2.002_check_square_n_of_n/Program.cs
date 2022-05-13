e3eb11d40e7dfb15bca9bd54d82e2779db727c06
afinyshka <afinyshka@mail.ru>
2022-05-13 23:43:05 +0300

    add 2.002 check square numbers file

---
+++ HW_2.002_check_square_n_of_n/Program.cs
@@ -0,0 +1,11 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого:
+Console.WriteLine("Enter first number:");
+int n1 = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Enter second number:");
+int n2 = int.Parse(Console.ReadLine() ?? "0");
+if (n1 == n2*n2 || n2 == n1*n1)
+{
+    if (n1>n2) Console.WriteLine($"Number {n1} is a square of number {n2}. ");
+    else if (n2>n1) Console.WriteLine($"Number {n2} is a square of number {n1}. ");
+}
+ else Console.WriteLine($"None of the numbers {n1} and {n2} is a square of the other. ");
