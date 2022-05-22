fcf4a35e75b48b6090aec1003f9f263501601249
afinyshka <afinyshka@mail.ru>
2022-05-22 13:39:48 +0300

    add file 3.001-3.003 find squares cubes sum from 1 to N

---
+++ HW_3.001-3.003_squres_cubes_sum_fron_1_to_N/Program.cs
@@ -0,0 +1,21 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("Enter a number: ");
+int number = int.Parse(Console.ReadLine()?? "0");
+Console.WriteLine($"Table of squares of numbers from 1 to {number} is: ");
+for (int number1=1; number1<=number; number1++) Console.Write($"{number1*number1} ");
+//for (int number1=1; number1<=number; number1++) Console.WriteLine($"{number1}^2={number1*number1} ");
+Console.WriteLine();
+
+// Найти кубы чисел от 1 до N
+
+Console.WriteLine($"Table of cubes of numbers from 1 to {number} is: ");
+for (int number1=1; number1<=number; number1++) Console.Write($"{number1*number1*number1} ");
+//for (int number1=1; number1<=number; number1++) Console.WriteLine($"{number1}^3={number1*number1*number1} ");
+Console.WriteLine();
+
+// Найти сумму чисел от 1 до А
+
+int sum = 0;
+for (int number1=1; number1<=number; number1++) sum = sum + number1;
+Console.WriteLine($"Summa of numbers from 1 to {number} is: {sum} ");
