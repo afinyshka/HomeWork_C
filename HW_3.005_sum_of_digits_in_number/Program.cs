86a71b43a951cbeeeccb99354cd4f36c11ba740d
afinyshka <afinyshka@mail.ru>
2022-05-22 14:15:49 +0300

    add file 3.005 summa of digits

---
+++ HW_3.005_sum_of_digits_in_number/Program.cs
@@ -0,0 +1,8 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.WriteLine("Enter a number: ");
+int number = Math.Abs(int.Parse(Console.ReadLine()?? "0"));
+int digitsSum = 0;
+int numberFirst = number;
+for (digitsSum = 0; number>=1; number=number/10) digitsSum = digitsSum + number%10;
+Console.WriteLine($"Summa of digits in {numberFirst} equals {digitsSum} ");
