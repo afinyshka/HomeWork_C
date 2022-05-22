615aa401273921a2427e27189d7d4729106aea00
afinyshka <afinyshka@mail.ru>
2022-05-22 14:32:33 +0300

    add file 3.006 cubes of even numbers

---
+++ HW_3.006_Cubes_of_even_numbers/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.WriteLine("Enter a number: ");
+int number = int.Parse(Console.ReadLine()?? "0");
+if (number%2==0)
+Console.WriteLine($"Cube of number of {number} is: {number*number*number} ");
+else
+Console.WriteLine($"Number {number} is uneven! ");
