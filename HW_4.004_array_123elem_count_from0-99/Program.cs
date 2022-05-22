3419782443aad133e487a479bc454f71325b6001
afinyshka <afinyshka@mail.ru>
2022-05-22 17:15:55 +0300

    add 4.004 find count numbers from diaposon from 10 to 99 123elements-array

---
+++ HW_4.004_array_123elem_count_from0-99/Program.cs
@@ -0,0 +1,17 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int [] array123 = new int [123];
+Console.WriteLine("123-elements array: ");
+for (int i=0; i<array123.Length; i++) array123[i] = new Random().Next(0,501);
+for (int i=0; i<array123.Length; i++) Console.Write($"{array123[i]} ");
+Console.WriteLine();
+int countDiaposon = 0;
+for (int i = 0; i<array123.Length; i++)
+{
+    if (array123[i]>=10 && array123[i]<=99)
+    {
+        countDiaposon++;
+        //Console.WriteLine($"Element {i}: {array123[i]} ");
+    }
+}
+Console.WriteLine($"Number of array's elements from diaposon [10;99]: {countDiaposon} ");
