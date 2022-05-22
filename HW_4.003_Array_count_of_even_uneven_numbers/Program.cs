5966a489b4ecbb129639f808f2d6e1812a7c7b9d
afinyshka <afinyshka@mail.ru>
2022-05-22 19:26:05 +0300

    add 4.003 find even and uneven numbers in array

---
+++ HW_4.003_Array_count_of_even_uneven_numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+Console.WriteLine("Enter number of array elements: ");
+int number = int.Parse(Console.ReadLine()?? "0");
+int [] arrayFree = new int [number];
+Console.WriteLine($"{number}-elements array: ");
+for (int i=0; i<arrayFree.Length; i++) arrayFree[i] = new Random().Next(100,1000);
+for (int i=0; i<arrayFree.Length; i++) Console.Write($"{arrayFree[i]} ");
+// for (int i=0; i<arrayFree.Length; i++) Console.Write($"{new Random().Next(100,1000)} ");
+Console.WriteLine();
+
+int countEven = 0;
+int countUneven = 0;
+for (int i = 0; i<arrayFree.Length; i++)
+{
+    if (arrayFree[i]%2 != 0) countUneven++;
+    else if (arrayFree[i]%2 == 0) countEven++;
+}
+Console.WriteLine($"Number of even numbers: {countEven} ");
+Console.WriteLine($"Number of UNeven numbers: {countUneven} ");
