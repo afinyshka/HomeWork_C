25ec18be6f1f420bf852c39424269adbda2a3011
afinyshka <afinyshka@mail.ru>
2022-05-22 15:31:23 +0300

    add file 4.001 0 and 1 8elem array

---
+++ HW_4.001_Array_with_0_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+Console.WriteLine("8-elements array: ");
+int [] array8 = new int [8];
+for (int i=0; i<array8.Length; i++) array8[i] = new Random().Next(0,2);
+for (int i=0; i<array8.Length; i++) Console.Write($"{array8[i]} ");
+Console.WriteLine();
+
+// Mассив из N элементов, заполненных нулями и единицами вывести их на экран
+
+Console.WriteLine("Enter number of array elements: ");
+int number = int.Parse(Console.ReadLine()?? "0");
+int [] array00 = new int [number];
+Console.WriteLine($"{number}-elements array: ");
+for (int i=0; i<array00.Length; i++) Console.Write($"{new Random().Next(0,2)} ");
+Console.WriteLine();
