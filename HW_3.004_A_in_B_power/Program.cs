f6df2a9f0a4c8af0c26be05cfc706e878e5af49b
afinyshka <afinyshka@mail.ru>
2022-05-22 14:06:04 +0300

    add file 3.004 a in power b

---
+++ HW_3.004_A_in_B_power/Program.cs
@@ -0,0 +1,18 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.WriteLine("Enter a number: ");
+int number = int.Parse(Console.ReadLine()?? "0");
+Console.WriteLine("Enter the power of the number: ");
+int powerNumber = int.Parse(Console.ReadLine()?? "0");
+int power=1;
+if (powerNumber==0) Console.WriteLine($"Number {number} in power {powerNumber} equals {power} ");
+else if (powerNumber>0)
+{
+    for (int number1=1; number1<=powerNumber; number1++) power=power*number;
+    Console.WriteLine($"Number {number} in power {powerNumber} equals {power} ");    
+}
+else
+{
+    for (int number1=1; number1<=Math.Abs(powerNumber); number1++) power=power*number;
+    Console.WriteLine($"Number {number} in power {powerNumber} equals 1/{power} ");    
+}
