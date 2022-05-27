8677e5678f58f17426ab0d941d6759f9aa13f456
afinyshka <afinyshka@mail.ru>
2022-05-27 15:05:15 +0300

    add HW_6.002 file mn rule matrix

---
+++ HW_6.002_Matrix_a_mn_rule/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+int [,] FillMatrix (int rows, int columns)
+{
+    int [,] matrix = new int [rows, columns];
+    for (int i=0; i<rows; i++)
+    {
+        for (int j=0; j<columns; j++)
+        matrix [i,j] = i+j;
+    }
+    return matrix;
+}
+
+void PrintMatrix (int [,] matrix)
+{
+    for (int i=0; i<matrix.GetLength(0); i++)
+    {
+        for (int j=0; j<matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix [i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Enter amount of rows: ");
+int rowsNum = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Enter amount of columns: ");
+int columnsNum = int.Parse(Console.ReadLine() ?? "0");
+int [,] matrix62 = FillMatrix (rowsNum, columnsNum);
+PrintMatrix (matrix62);
