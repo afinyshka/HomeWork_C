669173a3d7a1a28e149f1ff2d85218b15d8a847d
afinyshka <afinyshka@mail.ru>
2022-05-27 15:28:19 +0300

    add HW_6.003 file change even indexs matrix

---
+++ HW_6.003_Matrix_even_index_change/Program.cs
@@ -0,0 +1,52 @@
+﻿// В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+
+int[,] FillMatrix(int rows, int columns, int minRandom, int maxRandom)
+{
+    int[,] matrix = new int[rows, columns];
+    var rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+            matrix[i, j] = rand.Next(minRandom, maxRandom);
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChandeEvenIndexsElem(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0) matrix[i, j] = matrix[i, j] * matrix[i, j];
+        }
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Enter amount of rows in the matrix: ");
+int rowsNum = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Enter amount of columns in the matrix: ");
+int columnsNum = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Enter minimun number of random diaposon: ");
+int minRand = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Enter maximum number of random diaposon: ");
+int maxRand = int.Parse(Console.ReadLine() ?? "0");
+int[,] matrix63 = FillMatrix(rowsNum, columnsNum, minRand, maxRand);
+Console.WriteLine();
+PrintMatrix(matrix63);
+ChandeEvenIndexsElem(matrix63);
+Console.WriteLine();
+PrintMatrix(matrix63);
