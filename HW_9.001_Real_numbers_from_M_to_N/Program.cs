1068c098c5e27033ac40948924b1eab2c7727fec
afinyshka <afinyshka@mail.ru>
2022-06-09 00:43:06 +0300

    add 9.001 file show numbers from M to N

---
+++ HW_9.001_Real_numbers_from_M_to_N/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+void NumbersFromMtoN (int minNumber, int maxNumber)
+{
+    Console.Write(maxNumber + " ");
+    if (minNumber==maxNumber) return;
+    else NumbersFromMtoN(minNumber, maxNumber-1);
+}
+
+NumbersFromMtoN(5,20);
+Console.WriteLine();
