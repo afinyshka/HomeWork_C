a155709ecc440ff2160f36eb832407d86f5b4326
afinyshka <afinyshka@mail.ru>
2022-06-09 00:43:59 +0300

    add 9.002 file sum from m to N

---
+++ HW_9.002_Sum_elems_from_M_to_N/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int SumFromMtoN(int n, int m)
+{
+    if (n == m) return m;
+    else return (SumFromMtoN(n, m - 1) + m);
+    
+}
+
+int summa = SumFromMtoN(10, 15);
+Console.WriteLine($"Summa of the numbers from M to N equals = {summa} ");
